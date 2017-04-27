003451210267575109027bcbcc61447cd0bd440d
Salamshaker <salamhaitham@gmail.com>
2017-04-27 13:17:01 +0100

    Fix up seeder to do 100 and remove the extra fuzz

--- database/seeds/UsersTableSeeder.php
+++ database/seeds/UsersTableSeeder.php
@@ -11,8 +11,6 @@ class UsersTableSeeder extends Seeder
      */
     public function run()
     {
-        factory(App\User::class, 10000)->create()->each(function ($u) {
-           // $u->posts()->save(factory(App\Post::class)->make());
-        });
+        factory(App\User::class, 100)->create();
     }
 }
